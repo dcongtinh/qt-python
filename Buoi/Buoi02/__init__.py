d9ee5a892316fe6807558dbb3d33a76b7f9cb4b4
Vinh Phuc Ta Dang <vinhphuctadang@gmail.com>
2020-12-22 11:49:01 +0700

    example for buoi2

---
+++ Buoi/Buoi02/__init__.py
@@ -0,0 +1,9 @@
+#
+# Import tất cả cả các bài tập có chứa class Window
+#
+from . import Bai01, Bai02, Bai03, Bai04, Bai05, Bai06, Bai07, Bai08, Bai09, Bai10, Bai11, Bai12
+
+#
+# (Optional): Tên buổi, nếu không có trường này sẽ mặc định hiện tên module (tên folder chứa file này)
+#
+name='Buổi 2'
