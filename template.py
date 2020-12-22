5a2cfd7e3cf756a3fa2ce637064ac1119b4cd080
Tinh Dao <dcongtinh@gmail.com>
2020-12-22 09:07:35 +0700

    add template

---
+++ template.py
@@ -0,0 +1,34 @@
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+import numpy as np
+import sys
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        # set the title
+        self.setWindowTitle("This is a title")
+        self.setGeometry(300, 200, 800, 600)
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing, True)
+
+        self.drawSomething(painter)
+
+    def drawCircle(self, painter):
+        # code here
+        pass
+
+
+if __name__ == '__main__':
+    # create pyqt5 app
+    App = QApplication(sys.argv)
+
+    # create the instance of our Window
+    window = Window()
+    window.show()
+    # start the app
+    sys.exit(App.exec())
